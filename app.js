cbe108db97c143c88b8621a903c7a445b81fdbc1
Akinboboye Akinwande <boboye771@gmail.com>
2022-02-12 22:05:39 +0100

    removed dotenv file

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const express=require("express")
+
+const app=express()
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server listening on port ${process.env.PORT}`)
+})
