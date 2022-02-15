58e9ed2154c09667bb3f8cba5ea301bd2e381415
Akinboboye Akinwande <boboye771@gmail.com>
2022-02-15 07:41:04 +0100

    added delete endpoint and started working on edit

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const Book=require("../models/bookModel")
+const {Op}=require("sequelize")
 
 
 module.exports.getAllBooks=async(req, res)=>{
@@ -25,6 +26,75 @@ module.exports.addBook=async(req, res)=>{
 
 
     }catch (err){
+        console.log(err)
+        res.json(err)
+
+    }
+}
+
+module.exports.findBook=async(req, res)=>{
+    try{
+        const {title, year}=req.query
+        let result=[]
+        if (title){
+            result=await Book.findAll({
+                where:{
+                    title:{
+                        [Op.like]:"%"+title+"%"
+                    }
+                }
+            })
+
+        }
+
+        if (year){
+            result=await Book.findAll({
+                where:{
+                    year:{
+                        [Op.eq]:year
+                    }
+                }
+            })
+        }
+
+        res.json(result)
+
+    }catch(err){
+        console.log(err)
+        res.json(err)
+    }
+
+}
+
+module.exports.updateBook=async(req, res)=>{
+    try{
+        await Book.update({
+            id:1
+        },{
+            where:{
+                id:null
+            }
+        })
+        res.json({msg:"Updated successfully"})
+
+    }catch(err){
+        console.log(err)
+        res.json(err)
+
+    }
+}
+
+
+module.exports.deleteBook=async(req, res)=>{
+    try{
+        await Book.destroy({
+            where:{
+                id:null
+            }
+        })
+        res.json({msg:"deleted successfully"})
+
+    }catch(err){
         console.log(err)
         res.json(err)
 
--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,9 +1,12 @@
 const express=require("express");
-const { getAllBooks, addBook } = require("../controllers/bookController");
+const { getAllBooks, addBook, findBook, updateBook, deleteBook } = require("../controllers/bookController");
 const router=express.Router()
 
 
 router.get("/", getAllBooks)
 router.get("/allbooks", getAllBooks)
 router.post("/addbook", addBook)
+router.get("/findbook", findBook)
+router.post("/updatebook", updateBook)
+router.post("/deletebook", deleteBook)
 module.exports=router;
